9b8bf7fb3ad2693b85eb8dddb74ccc254d6cc1d6
menezesalexandre-development <alexandremmnzssilva2509@gmail.com>
2024-02-02 15:13:59 +0000

    Update - Added página de vendas

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 import pandas as pd
-from flask import Flask
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route('/')
+def index(): 
+  return "API Index Page" 
+
+@app.route('/vendas')
+def vendas():
+  table = pd.read_csv('advertising.csv')
+  total_vendas = table['Vendas'].sum()
+  qtd_vendas = table['Vendas'].count()
+  media_vendas = table['Vendas'].mean()
+  resposta = {'Total de Vendas': total_vendas, 
+              'Quantidade de Vendas': qtd_vendas, 
+              'Média de Vendas': media_vendas}
+  return jsonify(resposta)
+
+
+app.run(host='0.0.0.0')
